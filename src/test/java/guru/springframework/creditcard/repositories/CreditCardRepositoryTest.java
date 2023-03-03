94cb111c719abf1649704bfd76bab0d4ecae7e07
christiancalefati <c.calefati@gmail.com>
2023-03-03 18:57:26 +0100

    - EncryptionInterceptor created - InterceptorRegistration class created

--- src/test/java/guru/springframework/creditcard/repositories/CreditCardRepositoryTest.java
+++ src/test/java/guru/springframework/creditcard/repositories/CreditCardRepositoryTest.java
@@ -6,7 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,6 +27,9 @@ class CreditCardRepositoryTest {
     @Autowired
     EncryptionService encryptionService;
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
 
     @Test
     void saveCreditCard() {
@@ -36,6 +42,14 @@ class CreditCardRepositoryTest {
         System.out.println("ENCRYPTED: " + encryptionService.encrypt(cc.getCreditCardNumber()));
 
         var savedCC = creditCardRepository.saveAndFlush(cc);
+
+        Map<String, Object> resultMap = jdbcTemplate.queryForMap("SELECT * FROM credit_card cc WHERE cc.id ="
+                + savedCC.getId());
+        String ccValue = (String) resultMap.get("credit_card_number");
+
+        assertThat(ccValue).isNotEqualTo(savedCC.getCreditCardNumber());
+        assertThat(ccValue).isEqualTo(encryptionService.encrypt(savedCC.getCreditCardNumber()));
+
         var fetchedCC = creditCardRepository.findById(savedCC.getId()).orElse(null);
         assertThat(fetchedCC.getCreditCardNumber().equals(savedCC.getCreditCardNumber())).isTrue();
     }
