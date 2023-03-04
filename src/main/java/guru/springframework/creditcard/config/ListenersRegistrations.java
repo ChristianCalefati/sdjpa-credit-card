fe8b74d06946934f5bb68c5f65cd039d49090992
christiancalefati <c.calefati@gmail.com>
2023-03-04 20:15:01 +0100

    - ListenersRegistrations added

---
+++ src/main/java/guru/springframework/creditcard/config/ListenersRegistrations.java
@@ -0,0 +1,36 @@
+package guru.springframework.creditcard.config;
+
+import guru.springframework.creditcard.listeners.PostLoadListener;
+import guru.springframework.creditcard.listeners.PreInsertListener;
+import guru.springframework.creditcard.listeners.PreUpdateListener;
+import lombok.AllArgsConstructor;
+import org.hibernate.event.service.spi.EventListenerRegistry;
+import org.hibernate.event.spi.EventType;
+import org.hibernate.internal.SessionFactoryImpl;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class ListenersRegistrations implements BeanPostProcessor {
+    private final PreUpdateListener preUpdateListener;
+    private final PreInsertListener preInsertListener;
+    private final PostLoadListener postLoadListener;
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if(bean instanceof LocalContainerEntityManagerFactoryBean){
+            LocalContainerEntityManagerFactoryBean lcemfb = (LocalContainerEntityManagerFactoryBean)  bean;
+            SessionFactoryImpl sessionFactory = (SessionFactoryImpl) lcemfb.getNativeEntityManagerFactory();
+            EventListenerRegistry registry = sessionFactory.getServiceRegistry().getService(EventListenerRegistry.class);
+
+            registry.appendListeners(EventType.POST_LOAD, postLoadListener);
+            registry.appendListeners(EventType.PRE_INSERT, preInsertListener);
+            registry.appendListeners(EventType.PRE_UPDATE, preUpdateListener);
+
+        }
+        return bean;
+    }
+}
--- src/main/java/guru/springframework/creditcard/listeners/PostLoadListener.java
+++ src/main/java/guru/springframework/creditcard/listeners/PostLoadListener.java
@@ -1,8 +1,11 @@
 package guru.springframework.creditcard.listeners;
 
 import org.hibernate.event.spi.PostLoadEvent;
+import org.hibernate.event.spi.PostLoadEventListener;
+import org.springframework.stereotype.Component;
 
-public class PostLoadEventListener  implements org.hibernate.event.spi.PostLoadEventListener {
+@Component
+public class PostLoadListener implements PostLoadEventListener {
     @Override
     public void onPostLoad(PostLoadEvent event) {
         System.out.println("I am on PostLoad Event Listener");
