33cc88e04a6bedec91d51a0a18a0e0e118f84efa
christiancalefati <c.calefati@gmail.com>
2023-03-04 19:33:20 +0100

    - EncryptionInterceptor disabled

--- src/main/java/guru/springframework/creditcard/interceptors/EncryptionInterceptor.java
+++ src/main/java/guru/springframework/creditcard/interceptors/EncryptionInterceptor.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-@Component
+//@Component
 @AllArgsConstructor
 public class EncryptionInterceptor extends EmptyInterceptor {
 
